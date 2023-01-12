b0932b6c64845827ff0b42a006c9f06668af9657
Udara-021 <smartexp7.uh@gmail.com>
2023-01-12 17:56:23 +0530

    Add project files.

---
+++ Areas/admin/adminAreaRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmicERP.Areas.admin
+{
+    public class adminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
